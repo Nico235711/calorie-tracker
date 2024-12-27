4f2221fa8c77e2b7597e7ca275ec4933a39570f3
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2024-12-27 14:18:21 -0300

    extrayendo la lógica del componente a un custom hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,66 +1,17 @@
-import { ChangeEvent, Dispatch, FormEvent, useEffect, useState } from "react"
+import { Dispatch } from "react"
 
 import { categories } from "../data/categories"
 import { ActivityActions, ActivityState, } from "../reducers/activity-reducer"
-import { Activity } from "../types"
-import { v4 as uuidv4 } from 'uuid'
+import { useForm } from "../hooks/useForm"
 
 type FormProps = {
   state: ActivityState
   dispatch: Dispatch<ActivityActions>
 }
 
-const initialState : Activity = {
-  id: uuidv4(),
-  category: 1,
-  name: "",
-  calories: 0
-}
-
 const Form = ({ state, dispatch }: FormProps) => {
-  const [activity, setActivity] = useState<Activity>(initialState)
 
-  useEffect(() => {
-    if (state.activeId) {
-      // obtengo la actividad con el mismo id
-      const selectedActivity = state.activities.filter(stateActivity => stateActivity.id === state.activeId)[0]
-      setActivity(selectedActivity)
-    }
-  }, [state.activeId]);
-
-  const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
-
-    const isNumberField = ["category", "calories"].includes(e.target.id)
-
-    // convierte un campo especifico a número
-    setActivity({
-      ...activity,
-      [e.target.id]: isNumberField ? +e.target.value : e.target.value
-    })
-  }
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    // disparo mi acción
-    dispatch({
-      type: "save-activity",
-      payload: {
-        newActivity: activity
-      }
-    })
-
-    setActivity({
-      ...initialState,
-      id: uuidv4()
-    })
-  }
-
-  const isValidActivity = () => {
-    const { name, calories } = activity
-    // trim() elimina los espacios al principio y al final
-    return name.trim() !== "" && calories > 0
-  }
+  const { activity, handleChange, isValidActivity, handleSubmit } = useForm(state, dispatch)
 
   return (
     <form
---
+++ src/hooks/useForm.ts
@@ -0,0 +1,65 @@
+import { ChangeEvent, Dispatch, FormEvent, useEffect, useState } from "react";
+import { Activity } from "../types";
+import { ActivityActions, ActivityState } from "../reducers/activity-reducer";
+import { v4 as uuidv4 } from 'uuid';
+
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: "",
+  calories: 0
+}
+
+export const useForm = (state: ActivityState, dispatch: Dispatch<ActivityActions>) => {
+
+  const [activity, setActivity] = useState<Activity>(initialState)
+
+  useEffect(() => {
+    if (state.activeId) {
+      // obtengo la actividad con el mismo id
+      const selectedActivity = state.activities.filter(stateActivity => stateActivity.id === state.activeId)[0]
+      setActivity(selectedActivity)
+    }
+  }, [state.activeId]);
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+
+    const isNumberField = ["category", "calories"].includes(e.target.id)
+
+    // convierte un campo especifico a número
+    setActivity({
+      ...activity,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value
+    })
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // disparo mi acción
+    dispatch({
+      type: "save-activity",
+      payload: {
+        newActivity: activity
+      }
+    })
+
+    setActivity({
+      ...initialState,
+      id: uuidv4()
+    })
+  }
+
+  const isValidActivity = () => {
+    const { name, calories } = activity
+    // trim() elimina los espacios al principio y al final
+    return name.trim() !== "" && calories > 0
+  }
+
+  return {
+    activity,
+    handleChange,
+    handleSubmit,
+    isValidActivity
+  }
+}
