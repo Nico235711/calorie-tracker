adb30fcd74fe75d7439e868952321fd64d44f4e6
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-01-10 16:14:04 -0300

    arreglando el diseño responsivo

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -22,7 +22,7 @@ const ActivityList = ({ activities, setActiveId, removeActivity }:  ActivityList
           activities.map(activity => (
             <div
               key={activity.id}
-              className="px-5 py-10 bg-white mt-5 flex justify-between shadow"
+              className="px-5 py-10 bg-white mt-5 flex flex-col md:flex-row md:justify-between shadow"
             >
               <div className="space-y-4 relative">
                 <p className={`absolute -top-8 -left-8 px-5 py-2 text-white uppercase font-bold ${activity.category === 1 ? `bg-lime-500` : `bg-orange-500`}`}>{categoryName(activity.category)}</p>
@@ -30,7 +30,7 @@ const ActivityList = ({ activities, setActiveId, removeActivity }:  ActivityList
                 <p className="text-4xl font-black text-lime-500">{activity.calories} Calorías</p>
               </div>
   
-              <div className="flex gap-5 items-center">
+              <div className="flex gap-5 items-center mt-5 md:mt-0">
                 <button onClick={() => setActiveId(activity.id)}
                 >
                   <PencilSquareIcon 
